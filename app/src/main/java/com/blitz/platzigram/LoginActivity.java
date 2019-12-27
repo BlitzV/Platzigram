21a8f3887282fc28d62b1d9eb104b439e04b3e4d
David Villegas <swatdavid1@gmail.com>
2019-12-27 18:09:01 -0400

    fix

--- app/src/main/java/com/blitz/platzigram/LoginActivity.java
+++ app/src/main/java/com/blitz/platzigram/LoginActivity.java
@@ -15,7 +15,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        //prueba de cambios nuevos
     }
 
     public void goCreateAccount(View view){
